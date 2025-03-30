ffa40eea0d05dad5645b84005974ad9c89eecab0
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-30 16:45:45 +0300

    Change favicon

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 import { Model } from "./model";
 
@@ -10,6 +10,7 @@ export default function Scene() {
     <>
       <p className="absolute inset-0 flex justify-center items-center text-center text-7xl sm:text-8xl lg:text-9xl font-extrabold font-dancing">Anas Attoum</p>
       <Canvas>
+        <OrbitControls/>
         <Environment preset="city" />
         <directionalLight intensity={3} position={[0, 3, 2]} />
         <Model />
