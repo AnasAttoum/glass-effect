6280056059a9f12979aca6c2dad1cd63c5523b85
Anas Attoum <anasattoum.12321@gmail.com>
2025-03-30 16:39:23 +0300

    Fix build warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Dancing_Script, Geist, Geist_Mono } from "next/font/google";
+import { Dancing_Script } from "next/font/google";
 import "./globals.css";
 
 const dancingScript = Dancing_Script({
